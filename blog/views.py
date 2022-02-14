0dd79badd6957604e66e7ffb8a2f875c58851d92
Manu Tome <manutome@gmail.com>
2022-02-14 20:51:46 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,14 @@ from blog.models import Post
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+import logging
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
-    return render(request, "blog/index.html", {"posts": posts})
+  logger.debug("Request index")
+  posts = Post.objects.filter(published_at__lte=timezone.now())
+  return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
