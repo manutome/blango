223e740fd053902dfd8792f5a1b42aab1f924a83
Manu Tome <manutome@gmail.com>
2022-02-15 22:35:40 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -20,6 +20,8 @@ class Dev(Configuration):
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
   BASE_DIR = Path(__file__).resolve().parent.parent
 
+  AUTH_USER_MODEL = "blango_auth.User"
+
   INTERNAL_IPS = ["192.168.10.226"]
 
   # Quick-start development settings - unsuitable for production
@@ -51,6 +53,7 @@ class Dev(Configuration):
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blango_auth',
     'blog',
     'crispy_forms',
     'crispy_bootstrap5'
